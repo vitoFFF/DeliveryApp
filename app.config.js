6ec554e1c42d83ec436702cd34891816eb5931bc
Vito Kvachadze <vitokvachadze@gmail.com>
2025-12-30 14:49:22 +0400

    Fix APK errors

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default ({ config }) => {
             config: {
                 ...config.android?.config,
                 googleMaps: {
-                    apiKey: process.env.GOOGLE_MAPS_API_KEY,
+                    apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
                 },
             },
         },
